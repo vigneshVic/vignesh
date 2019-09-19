a5784d254fe54067c22001781ebcdff5c52d7e54
vignesh <47426648+vigneshVic@users.noreply.github.com>
2019-09-19 17:14:56 +0530

    Create UserPolicy.php

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    public function before($user, $ability)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\User  $model
+     * @return mixed
+     */
+    public function view(User $user, User $model)
+    {
+        return $user->hasPermissionTo('view-users');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->hasPermissionTo('create-users');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\User  $model
+     * @return mixed
+     */
+    public function update(User $user, User $model)
+    {
+        return $user->hasPermissionTo('update-users');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\User  $user
+     * @param  \App\User  $model
+     * @return mixed
+     */
+    public function delete(User $user, User $model)
+    {
+        return $user->hasPermissionTo('delete-users');
+    }
+}
