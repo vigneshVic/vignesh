bccfc067c6ea613442fef9bb2c74a4830fcbd081
vignesh <47426648+vigneshVic@users.noreply.github.com>
2019-09-19 17:11:29 +0530

    Add files via upload

---
+++ database/migrations/2019_08_26_071049_create_comments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('comment');
+            $table->tinyInteger('status')->default(1);
+            $table->unsignedInteger('commentable_id');
+            $table->string('commentable_type');
+
+            $table->timestamps();
+            $table->softDeletes();
+            
+            $table->string('created_by');
+            $table->string('updated_by')->nullable();
+            $table->string('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
