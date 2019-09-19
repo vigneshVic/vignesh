53f77b521828378c6b8653eecb4e3b8ff7b130d5
vignesh <47426648+vigneshVic@users.noreply.github.com>
2019-09-19 17:08:54 +0530

    web.php

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+
+	Route::get('home', 'HomeController@index')->name('home');
+
+	Route::resource('user', 'UserController');
+
+	Route::resource('roles', 'RoleController');
+	
+	Route::resource('permissions', 'PermissionController');
+
+	Route::resource('post', 'PostController');
+	
+	Route::resource('comment', 'CommentController', ['only' => ['store', 'update', 'destroy']]);
+	Route::post('/reply/store', 'CommentController@replyStore')->name('reply.add');
+
+});
